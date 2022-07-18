d14cb17a8fe85ac0ffcec1386a92172e164d69ef
Neil Macneale <neil.macneale.v@gmail.com>
2022-07-18 11:11:29 -0700

    Added another failure test, which makes sure query failures cause exit code 1

--- tests/exitCodes.test.js
+++ tests/exitCodes.test.js
@@ -5,10 +5,18 @@ const assert = require('assert');
 // This test requires a local fauna container to be running.
 describe('deploy exit codes', () => {
   jest.setTimeout(30000);
+  // TODO: Start db here
+  // TODO: Make sure the db is empty here
   test('exit code 1', async () => {
-    await assert.rejects(async () => await exec("sls deploy -c failure.yml"), { "cwd": "./tests/config" });
+    await assert.rejects(async () => await exec("sls deploy -c fail-invalid-secret.yml", { "cwd": `${__dirname}/config` }));
   });
+  // TODO: Make sure the db is empty here
+  test('exit code 1', async () => {
+    await assert.rejects(async () => await exec("sls deploy -c fail-invalid-collection.yml", { "cwd": `${__dirname}/config` }));
+  });
+  // TODO: Make sure the db is empty here
   test('exit code 0', async () => {
-    await exec("sls deploy -c valid.yml", { "cwd": "./tests/config" });
+    await exec("sls deploy -c valid.yml", { "cwd": `${__dirname}/config` });
   });
+  // TODO: Make sure the db has a collection here
 });
