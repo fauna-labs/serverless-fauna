5232e34479807b54f98465b7258e45008899ebd8
Lucas Pedroza <lucas.pedroza@fauna.com>
2023-04-27 17:55:50 +0200

    Remove client cache

--- fqlx/client.js
+++ fqlx/client.js
@@ -1,19 +1,11 @@
 const { Client } = require('fauna')
 
-const clients = {}
 
 function getFQLXClient({ secret, endpoint }) {
-  const cacheKey = `${secret}/${endpoint}`
-
-  if (!clients[cacheKey]) {
-    clients[cacheKey] = new Client({
-      secret,
-      endpoint: new URL(endpoint),
-    })
-  }
-
-  return clients[cacheKey]
+  return new Client({
+    secret,
+    endpoint: new URL(endpoint),
+  })
 }
 
 module.exports = getFQLXClient
-module.exports.clients = clients
