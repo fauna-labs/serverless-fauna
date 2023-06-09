976abb98f04f191db183c0a6f2c80772b707a301
Lucas Pedroza <lucas.pedroza@fauna.com>
2023-06-09 16:12:40 +0200

    Fix --help

--- index.js
+++ index.js
@@ -25,82 +25,80 @@ class ServerlessFaunaPlugin {
       },
     };
 
-    // If --help is true, init with nothing and return
     if (options.help) {
+      // If --help is true, init with noops
       const cmd = new FaunaCommands({
-        deployCommand: null,
-        removeCommand: null,
+        deployCommand: async () => {},
+        removeCommand: async () => {},
       });
-      Object.assign(this.hooks, cmd.hooks);
-      Object.assign(this.commands.fauna.commands, cmd.command);
-      return;
-    }
-
-    this.config.fauna.version = this.config.fauna.version ?? "4";
-
-    if (
-      this.config.fauna.version === "10" ||
-      this.config.fauna.version === 10
-    ) {
-      this.serverless.configSchemaHandler.defineTopLevelProperty(
-        "fauna",
-        faunaV10Schema
-      );
-
-      const v10cmd = new FQL10Commands({
-        faunaClient: getV10Client(this.config.fauna.client),
-        serverless: this.serverless,
-        config: this.config.fauna,
-        options: this.options,
-        logger: this.logger,
-      });
-
-      const cmd = new FaunaCommands({
-        config: this.config,
-        deployCommand: v10cmd,
-        removeCommand: v10cmd,
-      });
-
-      Object.assign(this.hooks, cmd.hooks);
-      Object.assign(this.commands.fauna.commands, cmd.command);
-    } else if (
-      this.config.fauna.version === "4" ||
-      this.config.fauna.version === 4
-    ) {
-      this.serverless.configSchemaHandler.defineTopLevelProperty(
-        "fauna",
-        faunaV4Schema
-      );
-
-      const client = getV4Client(this.config.fauna.client);
-      const v4deploy = new FQL4DeployCommand({
-        faunaClient: client,
-        serverless: this.serverless,
-        config: this.config.fauna,
-        options: this.options,
-        logger: this.logger,
-      });
-
-      const v4remove = new FQL4RemoveCommand({
-        faunaClient: client,
-        serverless: this.serverless,
-        config: this.config.fauna,
-        options: this.options,
-        logger: this.logger,
-      });
-
-      const cmd = new FaunaCommands({
-        config: this.config,
-        deployCommand: v4deploy,
-        removeCommand: v4remove,
-      });
-
       Object.assign(this.hooks, cmd.hooks);
       Object.assign(this.commands.fauna.commands, cmd.command);
     } else {
-      throw new Error(
-        `Version must be '4' or '10', but was '${this.config.fauna.version}'`
-      );
+      this.config.fauna.version = this.config.fauna.version ?? "4";
+      if (
+        this.config.fauna.version === "10" ||
+        this.config.fauna.version === 10
+      ) {
+        this.serverless.configSchemaHandler.defineTopLevelProperty(
+          "fauna",
+          faunaV10Schema
+        );
+
+        const v10cmd = new FQL10Commands({
+          faunaClient: getV10Client(this.config.fauna.client),
+          serverless: this.serverless,
+          config: this.config.fauna,
+          options: this.options,
+          logger: this.logger,
+        });
+
+        const cmd = new FaunaCommands({
+          config: this.config,
+          deployCommand: v10cmd,
+          removeCommand: v10cmd,
+        });
+
+        Object.assign(this.hooks, cmd.hooks);
+        Object.assign(this.commands.fauna.commands, cmd.command);
+      } else if (
+        this.config.fauna.version === "4" ||
+        this.config.fauna.version === 4
+      ) {
+        this.serverless.configSchemaHandler.defineTopLevelProperty(
+          "fauna",
+          faunaV4Schema
+        );
+
+        const client = getV4Client(this.config.fauna.client);
+        const v4deploy = new FQL4DeployCommand({
+          faunaClient: client,
+          serverless: this.serverless,
+          config: this.config.fauna,
+          options: this.options,
+          logger: this.logger,
+        });
+
+        const v4remove = new FQL4RemoveCommand({
+          faunaClient: client,
+          serverless: this.serverless,
+          config: this.config.fauna,
+          options: this.options,
+          logger: this.logger,
+        });
+
+        const cmd = new FaunaCommands({
+          config: this.config,
+          deployCommand: v4deploy,
+          removeCommand: v4remove,
+        });
+
+        Object.assign(this.hooks, cmd.hooks);
+        Object.assign(this.commands.fauna.commands, cmd.command);
+      } else {
+        throw new Error(
+          `Version must be '4' or '10', but was '${this.config.fauna.version}'`
+        );
+      }
     }
   }
 }
